b6a5c49f114498c91f730cd76792a2954b3000aa
Ivan Solodilov <optim000000@gmail.com>
2024-07-22 02:11:23 +0400

    Add bazel BUILD

--- probe.c
+++ probe.c
@@ -29,7 +29,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 /****************************************************************************
  * Private Data
